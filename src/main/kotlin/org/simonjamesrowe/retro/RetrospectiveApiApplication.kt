c11147b39f6af474f8435f3de662340211417354
Simon Rowe <simon.rowe@gmail.com>
2020-04-11 20:54:56 +0100

    add in some spring cloud stream.

--- src/main/kotlin/org/simonjamesrowe/retro/RetrospectiveApiApplication.kt
+++ src/main/kotlin/org/simonjamesrowe/retro/RetrospectiveApiApplication.kt
@@ -1,29 +1,34 @@
 package org.simonjamesrowe.retro
 
 import com.mongodb.MongoClient
-import org.axonframework.eventhandling.EventBus
 import org.axonframework.eventsourcing.EventSourcingRepository
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine
 import org.axonframework.eventsourcing.eventstore.EventStore
 import org.axonframework.extensions.mongo.DefaultMongoTemplate
+import org.axonframework.extensions.mongo.MongoTemplate
 import org.axonframework.extensions.mongo.eventsourcing.eventstore.MongoEventStorageEngine
+import org.axonframework.extensions.mongo.eventsourcing.eventstore.StorageStrategy
+import org.axonframework.extensions.mongo.eventsourcing.tokenstore.MongoTokenStore
 import org.axonframework.modelling.command.Aggregate
 import org.axonframework.modelling.command.Repository
 import org.axonframework.serialization.Serializer
 import org.axonframework.spring.config.AxonConfiguration
-import org.axonframework.springboot.SerializerProperties
 import org.simonjamesrowe.retro.boards.Board
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.stream.annotation.EnableBinding
+import org.springframework.cloud.stream.messaging.Sink
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.support.KafkaHeaders
+import org.springframework.messaging.MessageHeaders
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
-import org.springframework.stereotype.Component
 
 
 @SpringBootApplication
+@EnableBinding(Sink::class)
 class RetrospectiveApiApplication
 
 fun main(args: Array<String>) {
@@ -46,6 +51,16 @@ class SecurityConfig {
 class EventSourcingConfiguration {
 
     @Bean
+    fun axonMongoTemplate(client: MongoClient) : MongoTemplate{
+        return DefaultMongoTemplate.builder()
+                .mongoDatabase(client.getDatabase("retro"))
+                .domainEventsCollectionName("retro_events")
+                .snapshotEventsCollectionName("retro_snapshots")
+                .trackingTokensCollectionName("retro_tracking_tokens")
+                .build()
+    }
+
+    @Bean
     fun eventStore(storageEngine: EventStorageEngine?, configuration: AxonConfiguration): EmbeddedEventStore? {
         return EmbeddedEventStore.builder()
                 .storageEngine(storageEngine)
@@ -54,19 +69,19 @@ class EventSourcingConfiguration {
     }
 
     @Bean
-    fun storageEngine(client: MongoClient, eventSerializer: Serializer): EventStorageEngine? {
-        return MongoEventStorageEngine.builder().mongoTemplate(DefaultMongoTemplate.builder()
-                .mongoDatabase(client.getDatabase("retro"))
-                .domainEventsCollectionName("retro_events")
-                .snapshotEventsCollectionName("retro_snapshots")
-                .trackingTokensCollectionName("retro_tracking_tokens")
-                .build())
+    fun mongoTokenStore(mongoTemplate: MongoTemplate, eventSerializer: Serializer) : MongoTokenStore{
+        return MongoTokenStore.builder().mongoTemplate(mongoTemplate).serializer(eventSerializer).build()
+    }
+
+    @Bean
+    fun storageEngine(mongoTemplate: MongoTemplate, eventSerializer: Serializer): EventStorageEngine? {
+        return MongoEventStorageEngine.builder().mongoTemplate(mongoTemplate)
                 .eventSerializer(eventSerializer)
                 .build()
     }
 
     @Bean
-    fun boardRepository(eventStore: EventStore) : Repository<Board> {
+    fun boardRepository(eventStore: EventStore): Repository<Board> {
         return EventSourcingRepository.builder(Board::class.java).eventStore(eventStore).build()
     }
 }
@@ -74,10 +89,23 @@ class EventSourcingConfiguration {
 class AggregateUtils {
 
     companion object {
-        fun <T> aggregateRoot (aggregate: Aggregate<T>) : T {
-            var t : T? = null
+        fun <T> aggregateRoot(aggregate: Aggregate<T>): T {
+            var t: T? = null
             aggregate.execute { t = it }
             return t!!
+        }
+    }
+}
+
+class KafkaUtils {
+
+    companion object {
+
+        fun messageMetaData(headers: MessageHeaders, index: Int): Map<String, String?> {
+            var messageMetaData = HashMap<String, String?>()
+            (headers.get(KafkaHeaders.BATCH_CONVERTED_HEADERS, List::class.java)?.get(index) as Map<String, ByteArray>)
+                    .forEach { (key, value) -> messageMetaData[key] = if (value == null) null else String(value)}
+            return messageMetaData
         }
     }
 }
--- src/main/kotlin/org/simonjamesrowe/retro/boards/Service.kt
+++ src/main/kotlin/org/simonjamesrowe/retro/boards/Service.kt
@@ -1,18 +1,28 @@
 package org.simonjamesrowe.retro.boards
 
 import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.eventhandling.EventHandler
 import org.axonframework.modelling.command.AggregateCreationPolicy
 import org.axonframework.modelling.command.AggregateLifecycle
 import org.axonframework.modelling.command.CreationPolicy
 import org.axonframework.modelling.command.Repository
 import org.simonjamesrowe.retro.AggregateUtils
+import org.simonjamesrowe.retro.KafkaUtils
+import org.slf4j.LoggerFactory
+import org.springframework.cloud.stream.annotation.StreamListener
+import org.springframework.cloud.stream.messaging.Sink
+import org.springframework.messaging.Message
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import java.util.*
 
 
 @Service
 class BoardCommandHandler(private val boardRepository: Repository<Board>) {
 
+    companion object {
+        val LOG = LoggerFactory.getLogger(BoardCommandHandler::class.java)
+    }
 
     @CommandHandler
     @CreationPolicy(AggregateCreationPolicy.ALWAYS)
@@ -21,6 +31,14 @@ class BoardCommandHandler(private val boardRepository: Repository<Board>) {
         aggregate.execute { AggregateLifecycle.apply(BoardCreatedEvent(it.id, createBoardCommand.name, createBoardCommand.createdBy, createBoardCommand.lanes)) }
         return AggregateUtils.aggregateRoot(aggregate)
     }
+
+    @StreamListener(target = Sink.INPUT)
+    fun consumeKafka(message: Message<List<String>>) {
+        message.payload.forEachIndexed{ index, value ->
+            LOG.info("Kafka message is ${value}, headers are ${KafkaUtils.messageMetaData(message.headers, index)}")
+        }
+    }
+
 
 
 }
