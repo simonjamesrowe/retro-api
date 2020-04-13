c150b01855683af7f841ed5b735a279723baad32
Simon Rowe <simon.rowe@gmail.com>
2020-04-13 22:24:59 +0100

    more advances

--- src/main/kotlin/org/simonjamesrowe/retro/boards/Model.kt
+++ src/main/kotlin/org/simonjamesrowe/retro/boards/Model.kt
@@ -1,30 +1,73 @@
 package org.simonjamesrowe.retro.boards
 
-import org.axonframework.eventhandling.EventHandler
+import org.axonframework.eventhandling.*
 import org.axonframework.eventsourcing.EventSourcingHandler
+import org.axonframework.messaging.Headers
+import org.axonframework.messaging.MetaData
 import org.axonframework.modelling.command.AggregateIdentifier
 import org.axonframework.modelling.command.AggregateLifecycle.apply
+import org.axonframework.modelling.command.AggregateMember
+import org.axonframework.modelling.command.EntityId
 import org.axonframework.spring.stereotype.Aggregate
+import org.slf4j.LoggerFactory
+import java.time.*
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
 
+const val CREATED_BY = "created-by"
+
+open class Command {
+    open var createdBy: String
+
+    constructor(createdBy: String) {
+        this.createdBy = createdBy
+    }
+}
+
 data class CreateBoardCommand(val name: String,
-                              val createdBy: String,
-                              val lanes: List<String>)
+                              override var createdBy: String,
+                              val lanes: List<String>) : Command(createdBy)
 
 data class BoardCreatedEvent(@AggregateIdentifier val id: String,
                              val name: String,
-                             val createdBy: String,
                              val lanes: List<String>)
 
+open class Entity {
+    var revision = 0L
+    lateinit var createdBy: String
+    lateinit var createdAt: ZonedDateTime
+    var updatedAt: ZonedDateTime? = null
+    var updatedBy: String? = null
+
+    fun doAudit(eventMessage: DomainEventMessage<Any>) {
+        Board.LOG.info("audit properties $eventMessage")
+        val timestamp = eventMessage.timestamp
+        val revision = eventMessage.sequenceNumber
+        if (revision == 0L) {
+            createdAt = ZonedDateTime.ofInstant(timestamp, ZoneOffset.UTC)
+            createdBy = eventMessage.metaData[CREATED_BY] as String
+        } else {
+            updatedAt = ZonedDateTime.ofInstant(timestamp, ZoneOffset.UTC)
+            updatedBy = eventMessage.metaData[CREATED_BY] as String
+        }
+        this.revision = revision
+    }
+}
+
 @Aggregate
-class Board {
+class Board : Entity {
+
+    companion object {
+        val LOG = LoggerFactory.getLogger(Board::class.java)
+    }
+
     @AggregateIdentifier
     lateinit var id: String
     lateinit var name: String
-    lateinit var createdBy : String
+
     var lanes: MutableMap<String, List<Card>> = HashMap()
+
 
     constructor()
 
@@ -33,16 +76,23 @@ class Board {
     }
 
     @EventSourcingHandler
-    fun on(boardCreatedEvent: BoardCreatedEvent) {
+    fun on(boardCreatedEvent: BoardCreatedEvent, eventMessage: DomainEventMessage<BoardCreatedEvent>) {
+        LOG.info("Handling $boardCreatedEvent")
         id = boardCreatedEvent.id
         name = boardCreatedEvent.name
-        createdBy = boardCreatedEvent.createdBy
-        boardCreatedEvent.lanes.forEach{
+        boardCreatedEvent.lanes.forEach {
             this.lanes[it] = ArrayList()
         }
+        doAudit(eventMessage as DomainEventMessage<Any>)
     }
 
 }
 
-data class Card(val id: String, val text: String,
-                val createdBy: String)
+
+class Card : Entity() {
+
+    @EntityId
+    lateinit var id: String
+    var text: String = ""
+
+}
--- src/main/kotlin/org/simonjamesrowe/retro/boards/Service.kt
+++ src/main/kotlin/org/simonjamesrowe/retro/boards/Service.kt
@@ -2,6 +2,7 @@ package org.simonjamesrowe.retro.boards
 
 import org.axonframework.commandhandling.CommandHandler
 import org.axonframework.eventhandling.EventHandler
+import org.axonframework.messaging.MetaData
 import org.axonframework.modelling.command.AggregateCreationPolicy
 import org.axonframework.modelling.command.AggregateLifecycle
 import org.axonframework.modelling.command.CreationPolicy
@@ -27,7 +28,8 @@ class BoardCommandHandler(private val boardRepository: Repository<Board>) {
     @CreationPolicy(AggregateCreationPolicy.ALWAYS)
     fun createBoard(createBoardCommand: CreateBoardCommand) : Board {
         var aggregate = boardRepository.newInstance { Board(UUID.randomUUID().toString()) }
-        aggregate.execute { AggregateLifecycle.apply(BoardCreatedEvent(it.id, createBoardCommand.name, createBoardCommand.createdBy, createBoardCommand.lanes)) }
+        aggregate.execute { AggregateLifecycle.apply(BoardCreatedEvent(it.id, createBoardCommand.name, createBoardCommand.lanes),
+                MetaData.with(CREATED_BY, "srowe")) }
         return AggregateUtils.aggregateRoot(aggregate)
     }
 
